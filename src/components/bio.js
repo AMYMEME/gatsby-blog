1dc4d190bf80486138c22898b124816cef53f20c
amy choi <cgc8016@ewhain.net>
2021-02-09 16:45:04 +0900

    style(bio, blog-post, layout): new style

--- src/components/bio.js
+++ src/components/bio.js
@@ -9,6 +9,8 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
 import { rhythm } from "../utils/typography"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../utils/fontawesome'
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -44,6 +46,7 @@ const Bio = () => {
         marginBottom: rhythm(2.5),
       }}
     >
+
       <Image
         fixed={data.avatar.childImageSharp.fixed}
         alt={author.name}
@@ -57,10 +60,30 @@ const Bio = () => {
           borderRadius: `50%`,
         }}
       />
-      <p>
-        Written by <strong>{author.name}</strong> {author.summary}
-        {` `}
-      </p>
+      <div>
+
+        <p style={{margin: 0}}>
+          Written by <strong>{author.name}</strong> {author.summary}
+        </p>
+        <div style={{display: `inline-flex`}}>
+          <a className={"social-icon-button"} href={`http://github.com/${social.github}`}
+             target={"_blank"} rel="noopener noreferrer">
+            <FontAwesomeIcon icon={['fab', 'github']} size={'lg'} style={{marginLeft: "0.3em", marginRight: "0.3em"}}/>
+            Github
+          </a>
+          <a className={"social-icon-button"} href={`https://www.linkedin.com/in/${social.linkedIn}`}
+             target={"_blank"} rel="noopener noreferrer">
+            <FontAwesomeIcon icon={['fab', 'linkedin-in']} size={'lg'} style={{marginLeft: "0.3em", marginRight: "0.3em"}}/>
+            LinkedIn
+          </a>
+          <a className={"social-icon-button"} href={`mailto:${social.gmail}?subject=Hello from website`}>
+            <FontAwesomeIcon icon={['fas', 'envelope']} size={'lg'} style={{marginLeft: "0.3em", marginRight: "0.3em"}}/>
+            Gmail
+          </a>
+        </div>
+
+      </div>
+
     </div>
   )
 }
