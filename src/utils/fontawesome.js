18a72f771b837cf50b2edc2ebff2c4c9a20647f1
amy choi <cgc8016@ewhain.net>
2021-02-09 16:43:55 +0900

    style(codeblock, fontawesome)

---
+++ src/utils/fontawesome.js
@@ -0,0 +1,8 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faGithub)
+library.add(faLinkedinIn)
+library.add(faEnvelope)
